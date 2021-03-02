31b668eeb8f57d96d7f5e9b55deca55acb2b9a0f
Ilion Beyst <ilion@dataroots.io>
2021-03-02 20:27:38 +0100

    add some comments to simplebot

--- simple.py
+++ simple.py
@@ -1,23 +1,32 @@
 import sys, json, random
 
 def move(command):
-    record = { 'moves': [command] }
-    print(json.dumps(record))
+    """ print a command record to stdout """
+    moves = []
+    if command is not None:
+        moves.append(command)
+
+    print(json.dumps({ 'moves': moves }))
+    # flush the buffer, so that the gameserver can receive the json-encoded line.
     sys.stdout.flush()
 
 for line in sys.stdin:
     state = json.loads(line)
-    # find planet with most ships
+    # you are always player 1.
     my_planets = [p for p in state['planets'] if p['owner'] == 1]
     other_planets = [p for p in state['planets'] if p['owner'] != 1]
 
     if not my_planets or not other_planets:
+        # we don't own any planets, so we can't make any moves.
         move(None)
     else:
+        # find my planet that has the most ships
         planet = max(my_planets, key=lambda p: p['ship_count'])
-        dest = min(other_planets, key=lambda p: p['ship_count'])
+        # find enemy planet that has the least ships
+        destination = min(other_planets, key=lambda p: p['ship_count'])
+        # attack!
         move({
             'origin': planet['name'],
-            'destination': dest['name'],
+            'destination': destination['name'],
             'ship_count': planet['ship_count'] - 1
         })
