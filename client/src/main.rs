1622e4e232cf95e051e2b666c4139e0b961ea34e
Ilion Beyst <ilion@dataroots.io>
2021-03-05 00:54:18 +0100

    implement new flow without tokenfile

--- client/src/main.rs
+++ client/src/main.rs
@@ -5,21 +5,17 @@ extern crate rand;
 extern crate hex;
 
 use hex::FromHex;
-use io::Write;
-use std::{io, io::Read};
+use std::io::{self, Read};
 use std::fs::File;
-use std::path::Path;
 use mozaic_core::Token;
 use mozaic_core::client::simple_client::{ClientParams, simple_client};
 use clap::{App, Arg};
-
-const DEFAULT_CONFIG_FILE: &'static str = "config.toml";
-
 
 #[derive(Serialize, Deserialize)]
 struct Config {
     server: String,
     frontend: String,
+    bot_token: Option<String>,
     bot_argv: Vec<String>,
 }
 
@@ -27,17 +23,19 @@ async fn run(config_path: &str) {
     let config = match read_config(&config_path) {
         Ok(config) => config,
         Err(err) => {
-            eprintln!("error reading {}:", config_path);
+            eprintln!("failed to read {}:", config_path);
             eprintln!("{}", err);
             return;
         }
     };
 
+    // TODO: add this when it actually works
+    // println!("Click to go to the lobby: {}/lobbies/hub?token={}", frontend, str_token);
 
-    let token = match get_token(config.frontend) {
+    let token = match get_token(&config) {
         Ok(token) => token,
         Err(err) => {
-            eprintln!("error fetching token:");
+            eprintln!("error parsing token:");
             eprintln!("{}", err);
             return;
         }
@@ -45,10 +43,9 @@ async fn run(config_path: &str) {
 
     let params = ClientParams {
         server: config.server,
-        token: token,
         argv: config.bot_argv,
+        token
     };
-    println!("connecting to server");
     simple_client(params).await;
 }
 
@@ -57,11 +54,16 @@ async fn main() {
     let matches = App::new("planetwars-client")
         .version("0.1")
         .author("Zeus WPI")
-        .arg(Arg::new("config")
-            .short('c')
-            .long("config")
-            .default_value(DEFAULT_CONFIG_FILE)
-            .takes_value(true))
+        .setting(clap::AppSettings::SubcommandRequiredElseHelp)
+        .setting(clap::AppSettings::VersionlessSubcommands)
+        .subcommand(App::new("run-client")
+            .about("Runs the planetwars client")
+            .long_about("Runs the planetwars client [aliases: run, client]")
+            .alias("run")
+            .alias("client")
+            .arg(Arg::new("config-file")
+                .about("config file to use")
+                .required(true)))
         .subcommand(App::new("generate-token"))
         .get_matches();
     
@@ -71,10 +73,12 @@ async fn main() {
             let str_token = hex::encode(&token);
             println!("{}", str_token);    
         }
-        _ => {
-            let config_path = matches.value_of("config").unwrap();
+        Some(("run-client", matches)) => {
+            let config_path = matches.value_of("config-file").unwrap();
             run(config_path).await;        
         }
+        None => { eprint!("no command specified") }
+        _ => unreachable!()
     }
 }
 
@@ -86,27 +90,17 @@ fn read_config(config_path: &str) -> Result<Config, io::Error> {
     return Ok(config);
 }
 
-fn get_token(frontend: String) -> Result<Token, io::Error> {
-    let token_file_path = Path::new("tokenfile");
-    if token_file_path.exists() {
-        let mut token_file = File::open(token_file_path)?;
-        let mut buf = String::new();
-        token_file.read_to_string(&mut buf)?;
-        // todo: return an error here instead of panicking
-        let token = Token::from_hex(buf)
-            .expect("failed to parse token");
-        let str_token = hex::encode(&token);
-        println!("using token {} from token file", str_token);
-        println!("Click to go to the lobby: {}/lobbies/hub?token={}", frontend, str_token);
-        return Ok(token);
+fn get_token(config: &Config) -> Result<Token, hex::FromHexError> {
+    if let Some(ref token_str) = config.bot_token {
+        Token::from_hex(token_str)
     } else {
+        // generate a temporary token
         let token: Token = rand::random();
-        let str_token = hex::encode(&token);
-        println!("using generated token {}", str_token);
-        println!("Click to go to the lobby: {}/lobbies/hub?token={}", frontend, str_token);
-        let mut token_file = File::create(token_file_path)?;
-        write!(token_file, "{}", hex::encode(&token))?;
-        println!("saved token to token file");
-        return Ok(token)
+        let token_str = hex::encode(&token);
+        println!("No token found in configuration file.");
+        println!("Using temporary token {}\n", token_str);
+        println!("To keep using this token, add the line below to your configuration file");
+        println!("bot_token = \"{}\"", token_str);
+        Ok(token)
     }
 }
